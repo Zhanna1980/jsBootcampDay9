49a2b8fd865785033e6bd34525ea3f59b460576e
Zhanna <zhanna.libman@gmail.com>
2017-01-25 15:29:05 +0200

    validation

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 /**
  * Created by zhannalibman on 25/01/2017.
  */
-var express = require('express')
-var app = express()
+var express = require('express');
+var server = express();
+var cache = [];
 
-app.get('/', function (req, res) {
+server.get('/', function (req, res) {
     res.sendFile("/Users/zhannalibman/targilim/jsBootcampDay9/index.html");
-})
+});
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080')
-})
+server.use('/form', function(req, res, next){
+    console.log('Someone try to access to /form');
+    next();
+});
+server.get('/form', function (req, res) {
+    const fieldsNotFilled = areAllFieldsFilled(req.query);
+    if (fieldsNotFilled.length > 0){
+        var response = "Not all fields are filled: ";
+        for (var i = 0; i < fieldsNotFilled.length; i++){
+            var comma = i === fieldsNotFilled.length - 1 ? "" : " ,";
+            response += fieldsNotFilled[i] + comma;
+        }
+        res.status(500).send(response);
+    } else {cache.push(req.query);
+        res.status(200).send(cache);
+    }
+});
+
+server.listen(8080, function () {
+    console.log('Example server listening on port 8080');
+});
+
+function areAllFieldsFilled(query){
+    var notFilled = [];
+    if(query.fullName.length == 0){
+        notFilled.push("Full name");
+    }
+    if(query.password.length == 0){
+        notFilled.push("Password");
+    }
+    if(query.pet === undefined){
+        notFilled.push("Pets");
+    }
+    if(query.about.length == 0){
+        notFilled.push("About");
+    }
+    return notFilled;
+
+}
+function next(){
+
+}
